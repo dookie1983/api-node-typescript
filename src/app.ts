378153e7d60076443e76d13eebae05b1b5ee127d
Ko Dookie <koh@odds.team>
2024-02-06 09:51:26 +0700

    refactor add mock data file

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { stock, getStocksSubset } from './stocks';
+import mockData from './mock-data';
 
 const app = express();
 const port = 3000;
 
-const allStock: stock[] = [
-    {
-        "id": 1,
-        "brand": "Apple",
-        "model": "iPhone 15 128GB",
-        "color": "GREEN",
-        "quantity": 100,
-        "promotion": "ลด 1,000 บาท เฉพาะ iPhone 15 Pro (128GB / 256GB / 512GB) เครื่องเปล่าเท่านั้น",
-        "imageUrl": "https://www.ais.th/content/dam/ais/consumer/store/devices/apple/iphone/iphone-15-series/iphone-15/product-detail/th/green/iphone-15-pdp-image-position-1a-green-color.jpg",
-    },
-    {
-        "id": 2,
-        "brand": "Apple",
-        "model": "iPhone 15 256GB",
-        "color": "BLUE",
-        "quantity": 200,
-        "promotion": "ลด 1,000 บาท เฉพาะ iPhone 15 Pro (128GB / 256GB / 512GB) เครื่องเปล่าเท่านั้น",
-        "imageUrl": "https://www.ais.th/content/dam/ais/consumer/store/devices/apple/iphone/iphone-15-series/iphone-15/product-detail/th/blue/iphone-15-pdp-image-position-1a-blue-color.jpg"    },
-    {
-        "id": 3,
-        "brand": "Apple",
-        "model": "iPhone 15 512GB",
-        "color": "YELLOW",
-        "quantity": 300,
-        "promotion": "ลด 1,000 บาท เฉพาะ iPhone 15 Pro (128GB / 256GB / 512GB) เครื่องเปล่าเท่านั้น",
-        "imageUrl": "https://www.ais.th/content/dam/ais/consumer/store/devices/apple/iphone/iphone-15-series/iphone-15/product-detail/th/yellow/iphone-15-pdp-image-position-1a-yellow-color.jpg",
-    },
-    {
-        "id": 4,
-        "brand": "Apple",
-        "model": "iPhone 15 Pro 128GB",
-        "color": "BLACK TITANIUM",
-        "quantity": 400,
-        "promotion": "ลด 1,000 บาท เฉพาะ iPhone 15 Pro (128GB / 256GB / 512GB) เครื่องเปล่าเท่านั้น",
-        "imageUrl": "https://www.ais.th/content/dam/ais/consumer/store/devices/apple/iphone/iphone-15-series/iphone-15-pro/product-detail/th/black-titanium/iphone-15-pro-pdp-image-position-1a-black-titanium-color.jpg"    },
-    {
-        "id": 5,
-        "brand": "Apple",
-        "model": "iPhone 15 Pro 256GB",
-        "color": "WHITE TITANIUM",
-        "quantity": 500,
-        "promotion": "ลด 1,000 บาท เฉพาะ iPhone 15 Pro (128GB / 256GB / 512GB) เครื่องเปล่าเท่านั้น",
-        "imageUrl": "https://www.ais.th/content/dam/ais/consumer/store/devices/apple/iphone/iphone-15-series/iphone-15-pro/product-detail/th/white-titanium/iphone-15-pro-pdp-image-position-1a-white-titanium-color.jpg"    }
-];
+const allStock: stock[] = mockData.stocks;
 
 // Use CORS middleware
 app.use(cors());
